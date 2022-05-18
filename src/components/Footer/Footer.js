884b3202d078ba69ec4c26d0e42abf15aa105f08
2018lopez <2018119251@ub.edu.bz>
2022-05-17 19:41:56 -0600

    v.1.0

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href=''
+      className='link footer__link'
+    >
+      Created By Imer Lopez
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,72 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: '',
+  title: 'Dev',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Imer Lopez',
+  role: 'Computer Science Student and Part-time developer',
+  description:
+    'I’m interested in web development, mobile development and cyber security',
+  resume: 'https://drive.google.com/file/d/1gg61l7nj2SsVojflepImPADeGu4tzTL_/view?usp=sharing',
+  social: {
+    linkedin: 'https://linkedin.com/in/imer-lopez-98b39419',
+    github: 'https://github.com/imerlopez',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  // {
+  //   name: 'Project 1',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
+  // {
+  //   name: 'Project 2',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
+  // {
+  //   name: 'Project 3',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Vuejs',
+  'Reactjs',
+  'MongoDB',
+  'Linux',
+  'API',
+  'Git',
+  'Nodejs',
+  'Mysql',
+  'Active Directory',
+  'Photoshop'
+]
+
+const contact = {
+  
+  email: 'imerlopez538@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
