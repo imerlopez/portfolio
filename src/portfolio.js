da86d9e65f39f98f384f33834bc04ab9a2cb67f8
2018lopez <2018119251@ub.edu.bz>
2022-05-26 21:27:05 -0600

    update resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,32 +1,33 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: '',
-  title: 'Dev',
-}
+  homepage: "",
+  title: "Dev",
+};
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'Imer Lopez',
-  role: 'Computer Science Student and Part-time developer',
+  name: "Imer Lopez",
+  role: "Computer Science Student and Part-time developer",
   description:
-    'I’m interested in web development, mobile development and cyber security',
-  resume: 'https://drive.google.com/file/d/1gg61l7nj2SsVojflepImPADeGu4tzTL_/view',
+    "I’m interested in web development, mobile development and cyber security",
+  resume:
+    "https://drive.google.com/file/d/1bXSqKjWYJmkYa9n2hVzPNJBYVKOmM4Ni/view?usp=sharing",
   social: {
-    linkedin: 'https://linkedin.com/in/imer-lopez-98b39419',
-    github: 'https://github.com/imerlopez',
+    linkedin: "https://linkedin.com/in/imer-lopez-98b39419",
+    github: "https://github.com/imerlopez",
   },
-}
+};
 
 const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   // {
-  //   name: 'Project 1',
+  //   name: "Covid Tracker",
   //   description:
-  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-  //   stack: ['SASS', 'TypeScript', 'React'],
-  //   sourceCode: 'https://github.com',
-  //   livePreview: 'https://github.com',
+  //     "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+  //   stack: ["Vuejs", "Covid Api", "Vue Chartjs"],
+  //   sourceCode: "https://github.com/imerlopez/Covid19",
+  //   livePreview: "https://imerlopez.github.io/Covid19/#/",
   // },
   // {
   //   name: 'Project 2',
@@ -44,29 +45,30 @@ const projects = [
   //   sourceCode: 'https://github.com',
   //   livePreview: 'https://github.com',
   // },
-]
+];
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'Vuejs',
-  'Reactjs',
-  'MongoDB',
-  'Linux',
-  'API',
-  'Git',
-  'Nodejs',
-  'Mysql',
-  'Active Directory',
-  'Photoshop'
-]
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Vuejs",
+  "Reactjs",
+  "MongoDB",
+  "Linux",
+  "API",
+  "Git",
+  "Nodejs",
+  "Mysql",
+  "Active Directory",
+  "Photoshop",
+  "Figma",
+  "Express",
+];
 
 const contact = {
-  
-  email: 'imerlopez538@gmail.com',
-}
+  email: "imerlopez538@gmail.com",
+};
 
-export { header, about, projects, skills, contact }
+export { header, about, projects, skills, contact };
